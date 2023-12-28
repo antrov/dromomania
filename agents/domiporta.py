c2bbb51e6e5ea67b01e1efc3c2f576a2a98c96e8
Hubert Andrzejewski <antrof@gmail.com>
2023-12-28 17:32:55 +0100

    Base agent class

--- agents/domiporta.py
+++ agents/domiporta.py
@@ -1,5 +1,7 @@
 from urllib.parse import parse_qs, urlparse
+from selenium.webdriver.remote.webelement import WebElement
 from selenium import webdriver
+from selenium.webdriver.remote.webdriver import WebDriver as RemoteWebDriver
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
@@ -10,67 +12,103 @@ from enum import Enum
 
 QS_PAGE_KEY = 'PageNumber'
 
+
 class Browser(Enum):
     BRAVE = 1
     FIREFOX = 2
 
-def get_driver_brave():
-    options = ChromeOptions()
-    options.binary_location = '/Applications/Brave Browser.app/Contents/MacOS/Brave Browser'
-    # options.add_argument("--headless")
-    return webdriver.Chrome(options = options)
 
-def get_driver_firefox():
-    options = FirefoxOptions()
-    # options.add_argument("--headless")
-    return webdriver.Firefox(options=options)
+class ScrapAgent:
+    def get_driver_brave(self):
+        options = ChromeOptions()
+        options.binary_location = '/Applications/Brave Browser.app/Contents/MacOS/Brave Browser'
+        # options.add_argument("--headless")
+        return webdriver.Chrome(options=options)
 
+    def get_driver_firefox(self):
+        options = FirefoxOptions()
+        # options.add_argument("--headless")
+        return webdriver.Firefox(options=options)
 
-def get_driver(browser: Browser):
-    match browser:
-        case Browser.BRAVE:
-            return get_driver_brave()
-        case Browser.FIREFOX:
-            return get_driver_firefox()
+    def get_driver(self, browser: Browser):
+        match browser:
+            case Browser.BRAVE:
+                return self.get_driver_brave()
+            case Browser.FIREFOX:
+                return self.get_driver_firefox()
 
-driver = get_driver(Browser.FIREFOX)
+    def scroll_to_bottom(self, driver: RemoteWebDriver, step: int = 2):
+        total_height = driver.execute_script(
+            "return document.documentElement.scrollHeight")
+        for i in range(1, total_height, 1):
+            driver.execute_script("window.scrollTo(0, {});".format(i))
 
-def scrap(page: int):
-    driver.get(f'https://www.domiporta.pl/nieruchomosci/sprzedam/lodzkie/kolonia-ldzan?Distance=5&{QS_PAGE_KEY}={page}')
-    parsed_url = urlparse(driver.current_url)
-    qs = parse_qs(parsed_url.query)
-    if page > 1:
-        if QS_PAGE_KEY not in qs:
+    def url_is_on_page(self, url: str, page_key: str, expected_page: int) -> bool:
+        parsed_url = urlparse(url)
+        qs = parse_qs(parsed_url.query)
+        if page_key not in qs:
             print(f'No page key in url {qs}')
             return False
-        if not qs[QS_PAGE_KEY]:            
+        if not qs[page_key]:
             print('Page index is empty')
             return False
-        page_index = qs[QS_PAGE_KEY][0]
-        if page_index != str(page):
-            print(f'Page index {page_index} not match requested {page}')
+        page_index = qs[page_key][0]
+        if page_index != str(expected_page):
+            print(
+                f'Page index {page_index} not match expected {expected_page}')
+            return False
+        return True
+
+    def wait_page_ready(self, driver: RemoteWebDriver):
+        WebDriverWait(driver, 10).until(lambda driver:
+                                        driver.execute_script(
+                                            'return document.readyState') == 'complete'
+                                        )
+
+    def wait_for_elements(self, driver: RemoteWebDriver, css_selector: str) -> [WebElement]:
+        return WebDriverWait(driver, 60).until(EC.visibility_of_all_elements_located((By.CSS_SELECTOR, css_selector)))
+
+    def scrap_page(self, driver: RemoteWebDriver, url: str, page: int) -> bool:
+        return False
+
+    def scrap(self):
+        driver = self.get_driver(Browser.FIREFOX)
+        url = 'https://www.domiporta.pl/nieruchomosci/sprzedam/lodzkie/kolonia-ldzan?Distance=5'
+        page = 1
+        while self.scrap_page(driver, url, page):
+            page += 1
+
+        driver.close()
+
+
+class DomiportaAgent(ScrapAgent):
+    def scrap_page(self, driver: RemoteWebDriver, url: str, page: int) -> bool:
+        driver.get(f'{url}&{QS_PAGE_KEY}={page}')
+
+        if page > 1 and not self.url_is_on_page(driver.current_url, QS_PAGE_KEY, page):
             return False
 
-    total_height = driver.execute_script("return document.documentElement.scrollHeight")
-    for i in range(1, total_height, 2):
-        driver.execute_script("window.scrollTo(0, {});".format(i))
+        self.wait_page_ready(driver)
+        self.scroll_to_bottom(driver, 1)
 
-    # scrap
-    for my_elem in WebDriverWait(driver, 60).until(EC.visibility_of_all_elements_located((By.CSS_SELECTOR, "article.sneakpeak"))):
-        try:
-            pic_container = my_elem.find_element(By.CSS_SELECTOR, "a.sneakpeak__picture_container")
-            href = pic_container.get_attribute("href")
-            title = pic_container.get_attribute("title")
-            src = pic_container.find_element(By.CSS_SELECTOR, "img.loaded").get_attribute("src")
-            params = my_elem.find_element(By.CSS_SELECTOR, "div.sneakpeak__details").get_attribute("innerText")
-            print(f'{title} {params} - {href} -  {src}')
-        except NoSuchElementException as ex:
-            print(ex.msg)
+        # scrap
+        for my_elem in self.wait_for_elements(driver, "article.sneakpeak"):
+            try:
+                pic_container = my_elem.find_element(
+                    By.CSS_SELECTOR, "a.sneakpeak__picture_container")
+                href = pic_container.get_attribute("href")
+                title = pic_container.get_attribute("title")
+                src = pic_container.find_element(
+                    By.CSS_SELECTOR, "img.loaded").get_attribute("src")
+                param_items = []
+                for param_element in my_elem.find_elements(By.CSS_SELECTOR, "span.sneakpeak__details_item"):
+                    param = param_element.text.replace("\n", "").strip()
+                    if param:
+                        param_items.append(param)
+                params = ' - '.join(param_items)
+                print(f'{title} {params} - {href} -  {src}')
+            except NoSuchElementException as ex:
+                print(ex.msg)
 
-    return True
 
-page = 1
-while scrap(page):
-    page += 1   
-    
-driver.close()
+DomiportaAgent().scrap()
